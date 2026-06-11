ff3f41d968d6121c8c1df6a3f06fd3f7c7dc7e95
MaVanHai <12a4mavanhai@gmail.com>
2026-06-11 08:26:45 +0700

    cau hinh

--- CMS.Backend/Program.cs
+++ CMS.Backend/Program.cs
@@ -1,35 +1,3 @@
-//using Microsoft.EntityFrameworkCore;
-//using CMS.Data;
-
-//var builder = WebApplication.CreateBuilder(args);
-
-//// Add services to the container.
-//builder.Services.AddControllersWithViews();
-//builder.Services.AddDbContext<ApplicationDbContext>(options =>
-//    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
-//var app = builder.Build();
-
-//// Configure the HTTP request pipeline.
-//if (!app.Environment.IsDevelopment())
-//{
-//    app.UseExceptionHandler("/Home/Error");
-//    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-//    app.UseHsts();
-//}
-
-//app.UseHttpsRedirection();
-//app.UseStaticFiles();
-
-//app.UseRouting();
-
-//app.UseAuthorization();
-
-//app.MapControllerRoute(
-//    name: "default",
-//    pattern: "{controller=Home}/{action=Index}/{id?}");
-
-//app.Run();
 using CMS.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Authentication.Cookies;
@@ -60,6 +28,18 @@ builder.Services.AddAuthentication(
 
         options.SlidingExpiration = true;
     });
+builder.Services.AddAuthorization();
+// ---- C?U HÌNH CORS (THÊM VÀO TR??C builder.Build()) ----
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowReactApp", policy =>
+    {
+        policy.WithOrigins("http://localhost:3000") // Cho phép ReactJS ? port 3000 g?i t?i
+              .AllowAnyHeader()                     // Cho phép m?i lo?i Header (Content-Type, Authorization...)
+              .AllowAnyMethod()                     // Cho phép m?i ph??ng th?c HTTP (GET, POST, PUT, DELETE)
+              .AllowCredentials();                  // H? tr? truy?n Cookie/Session n?u c?n sau này
+    });
+});
 
 var app = builder.Build();
 
@@ -75,6 +55,12 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+app.UseStaticFiles();
+
+// Kích ho?t CORS ?úng v? trí này
+app.UseCors("AllowReactApp");
+
+//app.UseAuthorization();
 
 // Authentication ph?i ??ng tr??c Authorization
 app.UseAuthentication();
--- CMS.Backend/obj/Debug/net8.0/CMS.Backend.AssemblyInfo.cs
+++ CMS.Backend/obj/Debug/net8.0/CMS.Backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CMS.Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+19103d62f0d6c700ac49b25603d30da467d1e871")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88261bd536468826ec06e9b2df88598857a115b9")]
 [assembly: System.Reflection.AssemblyProductAttribute("CMS.Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CMS.Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
